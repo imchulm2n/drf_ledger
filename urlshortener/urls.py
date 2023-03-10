de58af3b3f0bf68f1a24ce53b6d9580cba43e9c8
imchulm2n <110436172+imchulm2n@users.noreply.github.com>
2023-03-11 04:46:59 +0900

    swagger

--- urlshortener/urls.py
+++ urlshortener/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('shortener/<int:pk>/', views.ShortenerAPIView.as_view()),
+    path('/<int:pk>/', views.ShortenerAPIView.as_view()),
     path('<slug:new_link>/', views.OriginalAPIView.as_view()),
 ]
